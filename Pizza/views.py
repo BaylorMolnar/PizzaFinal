487859755334b1eb0a16d3667f743f4dadc24c66
Baylor Molnar <baylor_molnar1@baylor.edu>
2021-04-29 10:53:00 -0500

    Pizzas page link

--- Pizza/views.py
+++ Pizza/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 # Create your views here.
+def index(request):
+    # the home page for pizzeria
+    return render(request, "Pizza/index.html")
+
+
+def pizzas(request):
+    pizzas = Pizza.objects.order_by("date_added")
+
+    context = {"pizzas": pizzas}
+    return render(request, "Pizza/pizzas.html", context)
