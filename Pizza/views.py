196630ef29ca035411152ab7f79566b711d9bbd1
Baylor Molnar <baylor_molnar1@baylor.edu>
2021-04-29 13:56:29 -0500

    Comments working

--- Pizza/views.py
+++ Pizza/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Pizza
+from .forms import CommentForm
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,29 @@ def pizzas(request):
 
     context = {"pizzas": pizzas}
     return render(request, "Pizza/pizzas.html", context)
+
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by("date_added")
+
+    context = {"pizza": pizza, "toppings": toppings}
+    return render(request, "Pizza/pizza.html", context)
+
+
+def new_comment(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    if request.method != "POST":
+        form = CommentForm()
+    else:
+        form = CommentForm(data=request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.pizza = pizza
+            new_comment.save()
+            form.save()
+            return redirect("Pizza:pizza", pizza_id=pizza_id)
+
+    context = {"form": form, "pizza": pizza}
+    return render(request, "Pizza/new_comment.html", context)
